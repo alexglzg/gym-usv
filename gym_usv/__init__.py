225d7d4e33ec84dd6cf82f9afd2b1c4550b6c28f
alexglzg <alex_gg97@hotmail.com>
2020-06-12 18:32:53 -0500

    usv-pid-v0 working

--- gym_usv/__init__.py
+++ gym_usv/__init__.py
@@ -1,11 +1,26 @@
 from gym.envs.registration import register
 
 register(
+    id='usv-v0',
+    entry_point='gym_usv.envs:UsvEnv'
+)
+register(
     id='usv-asmc-v0',
     entry_point='gym_usv.envs:UsvAsmcEnv'
+)
+
+register(
+    id='usv-pid-v0',
+    entry_point='gym_usv.envs:UsvPidEnv'
 )
 
 register(
     id='usv-asmc-ye-int-v0',
     entry_point='gym_usv.envs:UsvAsmcYeIntEnv'
 )
+
+register(
+    id='usv-obstacles-v0',
+    entry_point='gym_usv.envs:UsvObstaclesEnv'
+)
+
